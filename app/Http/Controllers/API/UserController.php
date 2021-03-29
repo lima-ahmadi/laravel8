ad8ed72b32e50ae0c214662c3b4af7aea16097f3
Freshta-Zaheer <raihanisahar@gmail.com>
2021-03-30 00:48:54 +0430

    initial commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::latest()->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $this->validate($request,[
+            
+            'name'=>'required|string|max:191',
+            'email'=>'required|string|email|max:191|unique:users',
+            'password'=>'required|string|min:8 '
+        ]);
+
+        return User::create([
+            'name'=>$request['name'],
+            'email'=>$request['email'],
+            'type'=>$request['type'],
+            'password'=>Hash::make($request['password']),
+            'bio'=>$request['bio'],
+            'photo'=>$request['photo']
+        ]);
+
+    }
+
+    public function updateProfile(Request $request){
+        
+
+        $user=  auth('api')->user();
+
+        $this->validate($request,[
+            
+            'name'=>'required|string|max:191',
+            'email'=>'required|string|email|max:191|unique:users,email,
+            '.$user->id,
+            'password'=>'sometimes|required|min:8 '
+        ]);
+       
+        $currentPhoto=$user->photo;
+        if($request->photo != $currentPhoto){
+
+            $name=time().'.'.explode('/',
+            explode(':',substr($request->photo,0,strpos($request->photo,';')))[1])[1];
+
+            \Image::make($request->photo)->save(public_path('images/profile/').$name);
+            
+            $request->merge(['photo'=>$name]);
+
+            $userPhoto=public_path('images/profile/').$currentPhoto;
+
+            if(file_exists($userPhoto)){
+                @unlink($userPhoto);
+            }
+        }
+
+
+        if(!empty($request->password)){
+
+            $request->merge(['password'=>Hash::make($request['password'])]);
+        }
+
+        $user->update($request->all());
+        return ['message' =>'Success'];
+        
+    }
+
+    
+    public function profile()
+    {
+        return auth('api')->user();
+    }
+
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $user=User::findOrFail($id);
+
+        $this->validate($request,[
+            
+            'name'=>'required|string|max:191',
+            'email'=>'required|string|email|max:191|unique:users,email,'.$user->id,
+            'password'=>'sometimes|min:8 '
+        ]);
+
+        $user->update($request->all());
+        return ['message'=>'ویرایش اطلاعات کاربر'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user=User::findOrFail($id);
+        $user->delete();
+        return ['message'=>'مطلب مورد نظر شما با موفقیت حذف'];
+    }
+}
